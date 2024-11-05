a0ba6ce337696d6d5ba8c4f057120ca66596a160
TomTPHoang <phuocthien268@gmail.com>
2024-11-04 19:05:00 -0600

    Added more in for Weaponry Section

--- Red Dead Redemption 2/obj/Debug/net8.0/Red Dead Redemption 2.AssemblyInfo.cs
+++ Red Dead Redemption 2/obj/Debug/net8.0/Red Dead Redemption 2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Red Dead Redemption 2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56ad0aa24e1a5f3c73079afc32b432e71958194a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Red Dead Redemption 2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Red Dead Redemption 2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
